c87152f73845aecaccb8ec12c21137e9b550a8e0
pedrokorwa <korwapedro@gmail.com>
2021-07-24 21:23:58 +0700

    new

--- Kotlin Generic/Generic Class/GenericClass.kt
+++ Kotlin Generic/Generic Class/GenericClass.kt
@@ -4,6 +4,7 @@ class GenericClass <T>(val data : T){
 
 fun main() {
     val dataString = GenericClass<String>("pedro")
+    val velueString : String = dataString.data
     val number = GenericClass<Int>(23)
     val boolean = GenericClass<Boolean>(true)
     val char = GenericClass<Char>('S')
