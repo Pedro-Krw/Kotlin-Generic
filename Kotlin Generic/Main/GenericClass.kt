fa511a0e51415d537d17a92a46452af3a0c1e089
pedrokorwa <korwapedro@gmail.com>
2021-07-27 19:50:46 +0700

    new

---
+++ Kotlin Generic/Main/GenericClass.kt
@@ -0,0 +1,22 @@
+class GenericClass <T>(val data : T){
+}
+
+fun main() {
+
+
+}
+
+class tampung{
+    val objectData = setDataGeneric()
+
+}
+
+
+
+private fun setDataGeneric(val data : <T>){
+
+
+
+
+
+}
