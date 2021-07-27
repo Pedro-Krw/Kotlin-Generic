de1208c31c1428d5b9efab92c10ebb08376f2852
pedrokorwa <korwapedro@gmail.com>
2021-07-27 21:37:57 +0700

    new

--- Kotlin Generic/src/GenericClass.kt
+++ Kotlin Generic/src/GenericClass.kt
@@ -4,10 +4,12 @@ class Data <T>(val data : T){
 }
 
 fun main() {
-
+ DataBind()
 
 }
 
  private fun DataBind(){
 
+     val dataString = Data<String>("This Program run")
+     println("ini Adalah hasil runya -> $dataString")
  }
