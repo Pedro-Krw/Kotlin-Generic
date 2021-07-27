3522a3d31fc4bcc3822e3cb99e214029daab8c71
pedrokorwa <korwapedro@gmail.com>
2021-07-27 19:59:41 +0700

    new

--- Kotlin Generic/Main/GenericFunction.kt
+++
@@ -1,8 +0,0 @@
-class fung{
-
-    fun main() {
-
-    }
-
-}
-
--- Kotlin Generic/Main/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-
-}
-
-  private fun setData(){
-      val name = "none"
-      val age = "none"
-      val array = "none"
-  }
