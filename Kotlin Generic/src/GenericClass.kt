b5ab61aa0b8fdc6c7cf761ecdbfc6b31e292558a
pedrokorwa <korwapedro@gmail.com>
2021-07-27 21:57:05 +0700

    new

--- Kotlin Generic/src/GenericClass.kt
+++ Kotlin Generic/src/GenericClass.kt
@@ -13,3 +13,5 @@ fun main() {
      val dataString = Data<String>("This Program run")
      println("ini Adalah hasil runya -> ${dataString.data}")
  }
+
+
---
+++ Kotlin Generic/src/Service.kt
@@ -0,0 +1,10 @@
+data class Service<T>(
+    val default : T ,
+
+
+)
+ private  fun setData(){
+     val dataString = Service<String>("This program is use generic class on data class")
+     val setup = dataString.default
+
+}
