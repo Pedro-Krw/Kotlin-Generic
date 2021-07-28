ece84802474cc89e5a29dd810d4cc0caf7ade79a
pedrokorwa <korwapedro@gmail.com>
2021-07-28 21:45:56 +0700

    new

--- Kotlin Generic/src/GenericFunction.kt
+++ Kotlin Generic/src/GenericFunction.kt
@@ -1,2 +1,14 @@
-class GenericFunction {
+class GenericFunction(val name : String) {
+
+    fun <T> sai(param : T){
+        print("Sai get one $param , and buy 2 from $name")
+    }
+
+
+}
+
+fun main() {
+
+    val objc = GenericFunction("")
+    objc.sai("run the project from jvm")
 }
